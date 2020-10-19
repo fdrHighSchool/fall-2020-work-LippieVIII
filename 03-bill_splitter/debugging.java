09ab1a6b17066fc52fe57982546d2de4001803a7
Kyler Li <kylerli@MacBook-Air.local>
2020-10-19 19:02:45 -0400

    Work4

---
+++ 03-bill_splitter/debugging.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class debugging {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to the Bill-Splitter");
+		System.out.print("Enter the total, pre-tax: ");
+
+		double total = input.nextInt();
+		System.out.print("How many people? ");
+
+		int numPeople = input.nextInt();
+		double perPerson = numPeople / total;
+
+		System.out.println("Each person must pay $" + perPerson + ", plus tip");
+	}//end main method
+}//end class
