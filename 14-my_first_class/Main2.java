81dd4792c5a17d194fbde2e838c778ad7022ff15
Kyler Li <kylerli@MacBook-Air.local>
2021-01-15 23:09:46 -0500

    Work14

---
+++ 14-my_first_class/Main2.java
@@ -0,0 +1,24 @@
+class Main2 {
+  public static void main(String[] args) {
+    Character character1 = new Character("Knight", "Physical attacks are stronger than average");
+    Character character2 = new Character("Mage", "Range abilities that either heal or damage others");
+    Character character3 = new Character("Archer", "Turning invisible");
+    Character character4 = new Character("Healer", "Heal everyone at once");
+    Character character5 = new Character("Thief", "Steal everyone's items");
+    Character character6 = new Character("Tank", "Resist any damage");
+    Character character7 = new Character("Shaman", "Enhance all of you and your teammates' stats.");
+    Character character8 = new Character("Necromancer", "Summon the dead to assist and heal you");
+    Character character9 = new Character("Scout", "Run faster at astronomical levels");
+    Character character10 = new Character("Builder", "Construct traps and explosives");
+    System.out.println(character1);
+    System.out.println(character2);
+    System.out.println(character3);
+    System.out.println(character4);
+    System.out.println(character5);
+    System.out.println(character6);
+    System.out.println(character7);
+    System.out.println(character8);
+    System.out.println(character9);
+    System.out.println(character10);
+  }
+}
